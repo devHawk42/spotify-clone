0b3244faaa582ce7047fd9f425811a3942bed64f
devHawk42 <devhawk42@gmail.com>
2019-06-24 13:54:35 -0300

    set progress bar on Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,15 +11,36 @@ class Footer extends Component {
                     <div className="now-playing-left">
                         <div className="cover-art"></div>
                         <div className="track-info">
-                            <span>Maps of Non-Existence</span>
-                            <span>Thank you scientist</span>
+                            <span className="track-info-name ellipsis-one-line">Maps of Non-Existence</span>
+                            <span className="track-info-artist ellipsis-one-line">Thank you scientist</span>
                         </div>
                     </div>
                     <div className="now-playing-center">
+                        <div className="player-controls">
+                            <div className="player-control-buttons">
+                                <button className="control-button">BACK</button>
+                                <button className="control-button">PLAY</button>
+                                <button className="control-button">FORWARD</button>
+                            </div>
+                            <div className="playback-bar">
+                                <div className="playback-bar__progress-time">0:00</div>
+                                
+                                <div className="progress-bar">
+                                    <div className="middle-align progress-bar__bg">
+                                        <div className="progress-bar__fg_wrapper">
+                                            <div className="progress-bar__fg" style={{'transform':'translateX(-100%);'}}></div>
+                                        </div>
+                                    </div>
+                                </div>
 
+                                <div className="playback-bar__progress-time">04:20</div>
+                            </div>
+                        </div>
                     </div>
                     <div className="now-playing-right">
-
+                        <div>
+                            <button>VOLUME</button>
+                        </div>
                     </div>
                 </div>
             </div>
