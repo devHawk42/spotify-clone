be4e1d359fbae685e44a2984e2d94b52548a4067
devHawk42 <devhawk42@gmail.com>
2019-07-02 00:00:21 -0300

    devProfile page

--- src/components/DevProfile/DevProfile.jsx
+++ src/components/DevProfile/DevProfile.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import './DevProfile.css';
 
 const DevProfile = () => (
-    <div className="profile-container">
-        <div className="profile-structure">
-            <div className="profile-pic-wrapper"></div>
-            <ul className="profile-links-wrapper">
-                <li className="profile-link">Website</li>
-                <li className="profile-link">Github</li>
-                <li className="profile-link">Details</li>
-            </ul>
-        </div>
+  <div className="profile-container">
+    <div className="profile-structure">
+      <div className="profile-pic-wrapper" />
+      <ul className="profile-links-wrapper">
+        <li className="profile-link">Website</li>
+        <a href="https://github.com/devHawk42/spotify-clone" rel="noopener noreferrer" target="_blank"><li className="profile-link">Github</li></a>
+        <li className="profile-link">Details</li>
+      </ul>
     </div>
+  </div>
 );
 
 export default DevProfile;
