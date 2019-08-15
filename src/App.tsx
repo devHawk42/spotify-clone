16ea04f64845341c8dc5c87721fe0fed2466b662
devHawk42 <devhawk42@gmail.com>
2019-08-14 23:59:16 -0300

    lint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import {
   Footer, Home, Navbar, DevProfile, Search, Library, MainArtist, MainAlbum
